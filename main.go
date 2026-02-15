245ccf39a3b4603a76552a99bd94057f1772089d
DavidIkov <postnikovdavid@yandex.ru>
2026-02-15 22:48:50 +0300

    Added html for all the stuff, and added images server.

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"remote_monitor/monitor"
-	"time"
 )
 
-func main() {
-	fmt.Printf("%+v\n", monitor.GetStaticPCData())
+type AllPCData struct {
+	StaticData  monitor.StaticPCData
+	DynamicData monitor.DynamicPCData
+}
 
-	for true {
-		fmt.Printf("%+v\n", monitor.GetDynamicPCData())
-		time.Sleep(1000 * time.Millisecond)
-	}
+var DynamicData monitor.DynamicDataUpdater
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("panel/template.html")
+	t.Execute(w, AllPCData{monitor.GetStaticPCData(), DynamicData.GetData()})
+}
+
+func main() {
+	DynamicData.Start(1000, 10)
+	fs := http.FileServer(http.Dir("./graphs"))
+	http.Handle("/graphs/", http.StripPrefix("/graphs/", fs))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
--- monitor/monitor.go
+++ monitor/monitor.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/user"
 	"runtime"
+	"sync"
+	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
@@ -22,9 +24,29 @@ type StaticPCData struct {
 	Username string
 }
 
+type ramInfo struct {
+	Total uint64
+	Used  float32
+}
+
 type DynamicPCData struct {
-	CPUUsage []float64
-	RAM      mem.VirtualMemoryStat
+	CPUUsage []float32
+	RAM      ramInfo
+}
+
+func GetAverageDynamicPCData(dataRange []DynamicPCData) DynamicPCData {
+	if len(dataRange) == 0 {
+		return DynamicPCData{}
+	}
+	avgData := dataRange[0]
+	for i := 1; i < len(dataRange); i++ {
+		for j, usage := range dataRange[i].CPUUsage {
+			avgData.CPUUsage[j] = (avgData.CPUUsage[j]*float32(i) + usage) / float32(i+1)
+		}
+		avgData.RAM.Total = uint64((float32(avgData.RAM.Total)*float32(i) + float32(dataRange[i].RAM.Total)) / float32(i+1))
+		avgData.RAM.Used = (avgData.RAM.Used*float32(i) + dataRange[i].RAM.Used) / float32(i+1)
+	}
+	return avgData
 }
 
 func GetStaticPCData() StaticPCData {
@@ -51,9 +73,13 @@ func GetStaticPCData() StaticPCData {
 }
 
 func GetDynamicPCData() DynamicPCData {
-	cpu_usage, cpu_usage_err := cpu.Percent(0, true)
+	cpu_usage64, cpu_usage_err := cpu.Percent(0, true)
 	if cpu_usage_err != nil {
 		fmt.Println("Error: ", cpu_usage_err)
+	}
+	cpu_usage := make([]float32, len(cpu_usage64))
+	for i, v := range cpu_usage64 {
+		cpu_usage[i] = float32(v)
 	}
 
 	ram, ram_err := mem.VirtualMemory()
@@ -61,6 +87,36 @@ func GetDynamicPCData() DynamicPCData {
 		fmt.Println("Error: ", ram_err)
 	}
 
-	return DynamicPCData{CPUUsage: cpu_usage, RAM: *ram}
+	return DynamicPCData{CPUUsage: cpu_usage, RAM: ramInfo{Total: ram.Total, Used: float32(ram.UsedPercent)}}
+}
 
+type DynamicDataUpdater struct {
+	mutex sync.Mutex
+	data  []DynamicPCData
+	// In milliseconds.
+	measureTime   uint32
+	measureAmount uint32
+}
+
+func (updater *DynamicDataUpdater) Start(measureTime, measureAmount uint32) {
+	updater.measureTime = measureTime
+	updater.measureAmount = measureAmount
+	updater.data = make([]DynamicPCData, measureAmount)
+
+	go func() {
+		for true {
+			data := GetDynamicPCData()
+			updater.mutex.Lock()
+			updater.data = updater.data[1:]
+			updater.data = append(updater.data, data)
+			updater.mutex.Unlock()
+			time.Sleep(time.Duration(measureTime) * time.Millisecond / time.Duration(measureAmount))
+		}
+	}()
+}
+func (updater *DynamicDataUpdater) GetData() DynamicPCData {
+	updater.mutex.Lock()
+	data := GetAverageDynamicPCData(updater.data)
+	updater.mutex.Unlock()
+	return data
 }
