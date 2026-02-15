062ecefedf5b938c1be6644dcdb27b3c2c100f90
DavidIkov <postnikovdavid@yandex.ru>
2026-02-15 13:00:30 +0300

    Added dynamic pc data.

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"remote_monitor/monitor"
+	"time"
 )
 
 func main() {
-	fmt.Printf("%+v\n", monitor.GetPCData())
+	fmt.Printf("%+v\n", monitor.GetStaticPCData())
+
+	for true {
+		fmt.Printf("%+v\n", monitor.GetDynamicPCData())
+		time.Sleep(1000 * time.Millisecond)
+	}
 }
--- monitor/monitor.go
+++ monitor/monitor.go
@@ -7,33 +7,60 @@ import (
 	"runtime"
 
 	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
-type CPUData struct {
+type StaticCPUData struct {
 	Name        string
 	CoresAmount int
 	Arch        string
 }
 
-type PCData struct {
-	CPU      CPUData
+type StaticPCData struct {
+	CPU      StaticCPUData
 	Hostname string
 	Username string
 }
 
-func GetPCData() PCData {
+type DynamicPCData struct {
+	CPUUsage []float64
+	RAM      mem.VirtualMemoryStat
+}
+
+func GetStaticPCData() StaticPCData {
 	hostname, hostname_err := os.Hostname()
 	if hostname_err != nil {
-		fmt.Println("Error:", hostname_err)
+		fmt.Println("Error: ", hostname_err)
 	}
 	username, username_err := user.Current()
 	if username_err != nil {
-		fmt.Println("Error:", username_err)
+		fmt.Println("Error: ", username_err)
 	}
 	cpuInfo, cpu_err := cpu.Info()
 	if cpu_err != nil {
-		fmt.Println("Error:", cpu_err)
+		fmt.Println("Error: ", cpu_err)
 	}
 
-	return PCData{CPU: CPUData{Name: cpuInfo[0].ModelName, CoresAmount: runtime.NumCPU(), Arch: runtime.GOARCH}, Hostname: hostname, Username: username.Username}
+	return StaticPCData{
+		CPU: StaticCPUData{
+			Name:        cpuInfo[0].ModelName,
+			CoresAmount: runtime.NumCPU(),
+			Arch:        runtime.GOARCH},
+		Hostname: hostname,
+		Username: username.Username}
+}
+
+func GetDynamicPCData() DynamicPCData {
+	cpu_usage, cpu_usage_err := cpu.Percent(0, true)
+	if cpu_usage_err != nil {
+		fmt.Println("Error: ", cpu_usage_err)
+	}
+
+	ram, ram_err := mem.VirtualMemory()
+	if ram_err != nil {
+		fmt.Println("Error: ", ram_err)
+	}
+
+	return DynamicPCData{CPUUsage: cpu_usage, RAM: *ram}
+
 }
